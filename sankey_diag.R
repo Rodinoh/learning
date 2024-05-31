f23f9b0cfd4aefcfed19f7d97babaf7090f27be5
Raymond Odinoh <oduorodinoh@gmail.com>
2024-05-31 10:08:14 +0300

    adding a sankey code

---
+++ sankey_diag.R
@@ -0,0 +1,13 @@
+#This is still for learning
+# Load required libraries
+library(networkD3)
+
+# Sample data
+data <- data.frame(
+  source = c("A", "B", "C", "A", "D", "E"),
+  target = c("D", "C", "E", "B", "E", "D"),
+  value = c(5, 10, 8, 15, 20, 12)
+)
+
+# Create a Sankey plot
+sankeyNetwork(Links = data, Nodes = data.frame(name = unique(c(data$source, data$target))))
